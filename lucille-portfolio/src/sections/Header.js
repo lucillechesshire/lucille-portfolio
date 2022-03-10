1c6243bc12b77ff5fb4497349cde54201f150b3c
lucillechesshire <lucillec98@gmail.com>
2022-03-10 03:21:02 -0800

    hamburger and logo

--- lucille-portfolio/src/sections/Header.js
+++ lucille-portfolio/src/sections/Header.js
@@ -1,33 +1,48 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import logo from "../assets/images/logo/luce-logo.png";
+import { HashLink } from "react-router-hash-link";
 
 function Header() {
   const { pathname } = useLocation();
   return (
-    <nav
-      className="header"
-      id="header"
-      style={{
-        backgroundColor:
-          pathname === "/javascript-game"
-            ? "#33b2f3"
-            : pathname === "/movie-database"
-            ? "black"
-            : "#f9f9f7",
-      }}
-    >
-      <ul>
-        <li>
-          <a href="#works">Works</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <input id="burger" type="checkbox" />
+
+      <label for="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
+      <nav
+        className="header"
+        id="header"
+        style={{
+          backgroundColor:
+            pathname === "/javascript-game"
+              ? "#33b2f3"
+              : pathname === "/movie-database"
+              ? "black"
+              : "#f9f9f7",
+        }}
+      >
+        <Link to="/">
+          <img className="logo" src={logo}></img>
+        </Link>
+        <ul>
+          <li>
+            <HashLink to="/#works">Works</HashLink>
+          </li>
+          <li>
+            <HashLink to="/#about">About</HashLink>
+          </li>
+          <li>
+            <HashLink to="/#contact">Contact</HashLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
